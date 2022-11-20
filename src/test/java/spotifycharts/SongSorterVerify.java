f6e832d95a7ddc31c48f6ece915af9bca6fa02e3
Jaden505 <jadenxxxl@gmail.com>
2022-11-20 12:04:55 +0100

    Add second unittest to SongSorterVerify

--- src/test/java/spotifycharts/SongSorterVerify.java
+++ src/test/java/spotifycharts/SongSorterVerify.java
@@ -12,12 +12,14 @@ import static org.junit.jupiter.api.Assertions.*;
 public class SongSorterVerify {
     private SongSorter songSorter;
     private List<Song> fewSongs;
+    private static Comparator<Song> rankingSchemeTotal;
 
     @BeforeEach
     void setup() {
         ChartsCalculator chartsCalculator = new ChartsCalculator(1L);
         this.songSorter = new SongSorter();
         fewSongs = new ArrayList(chartsCalculator.registerStreamedSongs(23));
+        rankingSchemeTotal = Song::compareByHighestStreamsCountTotal;
     }
 
     @Test
@@ -25,5 +27,12 @@ public class SongSorterVerify {
         Song temp = fewSongs.get(0);
         fewSongs = songSorter.swap(0, 1, fewSongs);
         assertEquals(temp, fewSongs.get(1));
+    }
+
+    @Test
+    void checkSongsSizeAfterSorts() {
+        int temp = fewSongs.size();
+        songSorter.topsHeapSort(fewSongs.size(), fewSongs, rankingSchemeTotal);
+        assertEquals(temp, fewSongs.size(), "Array size not the same after sorting on whole array");
     }
 }
